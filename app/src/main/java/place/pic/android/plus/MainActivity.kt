f46eedefa076150689e52cdd1f6fa71e7effbea7
jinsu4755 <parkjinsu4755@gmail.com>
2021-10-14 04:01:13 +0900

    init commit

---
+++ app/src/main/java/place/pic/android/plus/MainActivity.kt
@@ -0,0 +1,11 @@
+package place.pic.android.plus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
